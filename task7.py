3340065156d3a3e9dc8a71c8b11cdd3ad1b8229d
Дягилев Вадим Георгиевич <v.dyagilev@main.nsu.ru>
2024-10-12 14:35:08 +0700

    доделал до 10

--- task7.py
+++ task7.py
@@ -1,39 +1,13 @@
-# с клавиатуры вводятся числа, ввод завершается числом 0.
-# Определить минимальное из введённых чисел Фибоначчи.
-# Вывести "нет", если чисел Фибоначчи в последовательности нет.
-# Числа Фибоначчи – это последовательность чисел,
-# которая начинается с двух единиц и каждое следующее число
-# равно сумме двух предыдущих: 1, 1, 2, 3, 5, 8, 13, …
+import math
+a = int(input())
+b = 9999999999999999999999999999999
+while a != 0:
+    if math.sqrt(5*a**2+4) == int(math.sqrt(5*a**2+4)) or math.sqrt(5*a**2-4) == int(math.sqrt(5*a**2-4)):
+        if a < b:
+            b = a
+    a = int(input())
 
-def is_fibonacci(n):
-    """Проверяет, является ли число Фибоначчи."""
-    a, b = 1, 1
-    while a < n:
-        a, b = b, a + b
-    return a == n
-
-
-def main():
-    min_fib = float('inf')
-    found_fib = False
-
-    while True:
-        number = int(input("Введите число (0 для завершения): "))
-
-        if number == 0:
-            break
-
-        if is_fibonacci(number):
-            found_fib = True
-            if number < min_fib:
-                min_fib = number
-
-    if found_fib:
-        print("Минимальное число Фибоначчи:", min_fib)
-    else:
-        print("нет")
-
-
-if __name__ == "__main__":
-    main()
-
+if b == 9999999999999999999999999999999:
+    print('нет')
+else:
+    print(f'Минимальное число Фибоначчи: {b}')
